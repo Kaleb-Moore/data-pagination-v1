844bc91ccf3297c2b6bc52115fc7e3e0d11a109e
Kaleb-Moore <k4l3bmoore@gmail.com>
2021-06-21 05:51:30 -0600

    Update script.js
    Changed where it created the button and search bar in the HTML as it kept running and creating new bars when I tested in the console.  Fixed button styling as it wasn't wrapped in an LI tag for the style attributes to take affect. Reworked the querySelector for the active button. Created new list for the search feature and returned it to the page over the regular array.

--- js/script.js
+++ js/script.js
@@ -3,6 +3,15 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
+const header = document.querySelector('.header');
+header.insertAdjacentHTML('beforeend', `
+<label for="search" class="student-search">
+   <span>Search by name</span>
+   <input id="search" placeholder="Search by name...">
+   <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+</label>
+`)
+
 
 //Creates first page of students
 const showPage = (list, page) => {
@@ -12,6 +21,9 @@ const showPage = (list, page) => {
 
    const studentList = document.querySelector('.student-list');
    studentList.innerHTML = '';
+   if(list.length === 0) {
+      studentList.innerHTML = `<h1>This person could not be found, please try again</h1>`
+   }
 
    //Loops over data objects range and adds formatted info to li
    for (let i = 0; i < list.length; i++) {
@@ -44,17 +56,19 @@ const addPagination = (list) => {
    //Loops over page length and creates button per page needed
    for (let i = 1; i <= infoLength; i++) {
       linkList.insertAdjacentHTML('beforeend', `
+      <li>
       <button type="button">${i}</button>
+      </li>
       `)
    }
 
-   const activeButton = document.querySelectorAll('button')[0];
-   activeButton.className = 'active';
+   document.querySelector('.link-list button').className = 'active';
+
    //Listens for click and changes target to active, removing active from previous button
    linkList.addEventListener('click', (e) => {
       if(e.target.tagName === "BUTTON") {
-         const unassign = document.querySelector('.active');
-         unassign.className = '';
+         const active = document.querySelector('.active');
+         active.className = '';
          e.target.className = 'active';
          showPage(list, e.target.textContent);
       }
@@ -62,17 +76,44 @@ const addPagination = (list) => {
  }
 
 //Creates and formats search bar 
-const searchBar = () => {
-   const header = document.querySelector('.header');
-   header.insertAdjacentHTML('beforeend', `
-   <label for="search" class="student-search">
-      <span>Search by name</span>
-      <input id="search" placeholder="Search by name...">
-      <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
-   </label>
-   `)
+const searchBar = (list) => {
+   const search = document.querySelector('#search');
+   const button = document.querySelectorAll('button')[0];
+   const input = document.querySelector('input');
+
+   const performSearch = (searchInput, list) => {
+      let searchList = [];
+      for (let i = 0; i < list.length; i++) {
+         const nameCheck = `${list[i].name.first.toLowerCase()} ${list[i].name.last.toLowerCase()}`
+         if (searchInput.length === 0) {
+            showPage(list, 1);
+            addPagination(list);
+         } else if (nameCheck.includes(searchInput.value.toLowerCase())) {
+            searchList.push(list[i]);
+            showPage(searchList, 1);
+            addPagination(searchList);
+         } 
+         // else if (searchInput.value.toLowerCase() !== nameCheck) {
+         //    document.querySelector('body').innerHTML = `Sorry, ${searchInput.value} is not a student`
+         // }
+      }
+   }
+
+   button.addEventListener('click', (e) => {
+      performSearch(search, data);
+   });
+
+
+   input.addEventListener('keyup', (e) => {
+      performSearch(search, data)
+   });
 }
 
-searchBar();
+
+// for (let i = 0; i < data.length; i++){
+//    console.log(data[i].name.first);
+//    console.log(data[i].name.last);
+// }
+searchBar(data);
 showPage(data, 1);
 addPagination(data);
